3e1f8a948e7c0ad6b14f18dbe54fbc3aa3549dff
gidroadmin <maindmn@gmail.com>
2021-08-30 19:05:57 +0300

    Work with objects

--- test.js
+++ test.js
@@ -82,37 +82,105 @@ array = [
   },
 ];
 
-// array[0].edges.top = { edgeTypeId: 7, type: "outside" };
-// console.log(array)
-let newArray = [];
-let firstRight = array[0].edges.right.edgeTypeId;
-function rightArray(array) {
-  // console.log(array.length);
-  // console.log(firstRight);
-  for (let i = 0; i < array.length; i++) {
-    if (array[i].edges.left === null) {
-      array[i].edges.left = { edgeTypeId: 0, type: "0" }
+
+// let newArray = [];
+// let firstRight = array[0].edges.right.edgeTypeId;
+// let first = array[0].edges.right.type;
+// console.log(firstRight, first)
+// function arrayWithOutNull(array) {
+//   // console.log(array.length);
+//   // console.log(firstRight);
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i].edges.left === null) {
+//       array[i].edges.left = { edgeTypeId: 0, type: "0" }
+//     }
+//     if (array[i].edges.top === null) {
+//       array[i].edges.top = { edgeTypeId: 0, type: "0" }
+//     }
+//     if (array[i].edges.right === null) {
+//       array[i].edges.right = { edgeTypeId: 0, type: "0" }
+//     }
+//     if (array[i].edges.bottom === null) {
+//       array[i].edges.bottom = { edgeTypeId: 0, type: "0" }
+//     }
+//   }
+//   return array
+// }
+// newArray = arrayWithOutNull(array);
+// arrayWithOutNull(array);
+
+for (let i = 0; i < array.length; i++) {
+  const element = array[i];
+
+  function getFiniteValue(element) {
+    getProp(element);
+
+    function getProp(o) {
+      for (var prop in o) {
+        if (typeof (o[prop]) === 'object') {
+          getProp(o[prop]);
+        } else {
+          if (o[prop] === 7 && o[prop] === 'inside') {
+            console.log(i);
+          }
+          console.log(prop, ' ', o[prop]);
+        }
+
+      }
+
     }
-    if (array[i].edges.top === null) {
-      array[i].edges.top = { edgeTypeId: 0, type: "0" }
-    }
-    if (array[i].edges.right === null) {
-      array[i].edges.right = { edgeTypeId: 0, type: "0" }
-    }
-    if (array[i].edges.bottom === null) {
-      array[i].edges.bottom = { edgeTypeId: 0, type: "0" }
-    }
+
   }
-  return array
+  getFiniteValue(element);
 }
-newArray = rightArray(array);
+
+
 // console.log(newArray);
 
-let newArray2 = newArray.filter(item => {
-  if (item.edges.bottom.edgeTypeId === 7) {
-    return true;
-  }
-})
-console.log(newArray2[0].id)
-rightArray(array);
-// console.log(newArray);
+// let newArray2 = newArray.filter(item => {
+//   if (item.edges.bottom.edgeTypeId === 7 && item.edges.bottom.type !== first) {
+//     return true;
+//   }
+// })
+// console.log(newArray2)
+
+
+// var results = [];
+// var searchField = "edgeTypeId";
+// var searchVal = "7";
+
+// for (let key in newArray[0].edges) {
+//   console.log(newArray[0].edges[key])
+//   // if (newArray[0].edges[key].edgeTypeId === 7) {
+//   //   return newArray[0];
+//   // }
+//   // for (var i = 0; i < newArray[0].edges.length; i++) {
+//   //   if (newArray[0].edges[i][searchField] === top) {
+//   //     results.push(newArray[0].edges[i]);
+//   //   }
+//   // }
+// }
+// function search(newArray33) {
+//   // console.log(newArray33.edges)
+//   for (let i = 0; i < 4; i++) {
+//     // console.log(newArray33.edges[i])
+//     // if (newArray33.edges[i].edgeTypeId === 7) {
+//     //   // results.push(newArray[0].edges[i]);
+//     //   console.log(newArray.edges);
+//     // }
+//   }
+// }
+// search(newArray[0]);
+// console.log(results);
+
+// for (var i = 0; i < 4; i++) {
+//   // console.log(newArray[0].edges);
+//   for (key in newArray[0].edges) {
+//     if (newArray[0].edges[key] === 'top') {
+//       //   results.push(objects[i]);
+//       console.log('coool');
+//     }
+//   }
+// }
+
+// getFiniteValue(array);
